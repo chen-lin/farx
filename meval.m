16940efde2a84596cc5cbc49357f00971965f2ed
Lin Chen <l.chen.tj@gmail.com>
2018-03-17 12:26:15 +0000

    Update meval.m

--- meval.m
+++ meval.m
@@ -1,6 +1,5 @@
 function [ fit, mae, mse, maxae ] = meval( y, y_hat )
-%MEVAL Summary of this function goes here
-%   Detailed explanation goes here
+%MEVAL calculate model evaluation indexes
     fit   = 100*(1-norm(y_hat-y)/norm(y-mean(y)));
     mae   = mean(abs(y-y_hat));
     maxae = max(abs(y-y_hat));
